9dead065731d1f31b45ec694838d697032cfbd7c
Anton Sander <mail@antonsander.de>
2023-12-12 15:41:44 +0100

    Finalcode day1_2.sh

--- day1_2.sh
+++ day1_2.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
-curl -s -b "session=...." https://adventofcode.com/2023/day/1/input | 
-tr ' ' '\n' | 
-awk '{
-  x=$0
-  gsub(/one/,"1")
-  gsub(/two/,"2");gsub(/three/,"3");gsub(/four/,"4");gsub(/five/,"5");gsub(/six/,"6");gsub(/seven/,"7");gsub(/eight/,"8");gsub(/nine/,"9");y=$0;gsub(/[^0-9]/,"")
-  a=substr($0,0,1);c=length($0);b=substr($0,c,1);d=a+''+b;sum += d; print NR,x,y,d,sum
-} 
-END {print sum}'
+input=$(curl -s -b "session=...." https://adventofcode.com/2023/day/1/input) 
 
+#
+#
+#
+for el in "$input"; do #  $@; do 
 
+sum=0
 for el in $@; do 
-	echo $el
+	w1=0 # echo -n $el" "
+	for (( i = 0; i < ${#el}; i++ )); do
+ 		w1=$(echo ${el:$i} | awk '
+   			BEGIN { split("one two three four five six seven eight nine",n," ")}
+      			{for (i=1; i<10; i++) { if ( substr($0,1,1) == i || index($0,n[i]) == 1 ) { print i; break } } }
+   		')
+   		if [ -n "$w1" ]; then break; fi # echo -n $w;break; fi
+ 	done
+ 	w2=0
 	for (( i = ${#el}-1; i >= 0; i-- )); do 
-		w=$(echo ${el:$i} | awk '
+		w2=$(echo ${el:$i} | awk '
 		  BEGIN { split("one two three four five six seven eight nine",n," ")}
-		  {for (i=2; i<10; i++) { if ( substr($0,1,1) == i || index($0,n[i]) == 1 ) { print i; break } } }
-		  ')
-           if [ -n "$w" ]; then echo $w;break; fi
+		  {for (i=1; i<10; i++) { if ( substr($0,1,1) == i || index($0,n[i]) == 1 ) { print i; break } } }
+		')
+           if [ -n "$w2" ]; then break; fi # echo $w;break; fi
 	done
+	sum=$((sum+=w1*10+w2))
+
+done
+echo "Sum: "$sum
+ 
 done
 
